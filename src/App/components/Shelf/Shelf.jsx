2d6173be6b16652c65f75650fbe1c2c248c0861b
theredwillow <theredwillows@gmail.com>
2017-12-30 14:39:35 -0600

    Added edges and made the shelf one sided so it can go against walls or be put back to back without making the video array mess up

--- src/App/components/Shelf/Shelf.jsx
+++ src/App/components/Shelf/Shelf.jsx
@@ -10,6 +10,25 @@ export class Shelf extends Component {
       x: 0,
       z: -3
     };
+
+    this.ledges = [];
+    for (let l = 0.025; l <= 1.9; l = l + 0.45) {
+      this.ledges.push( (
+        <a-entity key={l}>
+          <a-box class="shelf-ledge" color="purple"
+            depth="0.23" height="0.05" width={this.dimensions.width}
+            position={ [this.dimensions.x, l, -2.876].join(" ") }
+          >
+          </a-box>
+          <a-box class="shelf-edge" color="green"
+          depth="0.01" height="0.08" width={this.dimensions.width}
+          position={ [this.dimensions.x, (l + 0.015), -2.755].join(" ") }
+          >
+          </a-box>
+        </a-entity>
+      ) );
+    }
+
   }
 
   render() {
@@ -17,52 +36,24 @@ export class Shelf extends Component {
       <a-entity class="shelf">
 
         <a-box class="shelf-panel" color="red"
-          depth="0.05" height="2.25" width={this.dimensions.width}
+          depth="0.025" height="2.25" width={this.dimensions.width}
           position={ [this.dimensions.x, 1.125, this.dimensions.z].join(" ") }
         >
         </a-box>
 
         <a-box class="shelf-wing" color="blue"
-          width="0.05" height="2.25" depth="0.5"
-          position={ [(this.dimensions.width/2 + 0.025), 1.125, -3].join(" ") }
+          width="0.05" height="2.25" depth="0.26"
+          position={ [(this.dimensions.width/2 + 0.025), 1.125, -2.88].join(" ") }
         >
         </a-box>
 
         <a-box class="shelf-wing" color="yellow"
-          width="0.05" depth="0.5" height="2.25"
-          position={ [this.dimensions.x-(this.dimensions.width/2 + 0.025), 1.125, -3].join(" ") }
+          width="0.05" height="2.25" depth="0.26"
+          position={ [this.dimensions.x-(this.dimensions.width/2 + 0.025), 1.125, -2.88].join(" ") }
         >
         </a-box>
 
-        <a-box class="shelf-ledge" color="purple"
-          depth="0.5" height="0.05" width={this.dimensions.width}
-          position={ [this.dimensions.x, 0.025, -3].join(" ") }
-        >
-        </a-box>
-
-        <a-box class="shelf-ledge" color="purple"
-          depth="0.5" height="0.05" width={this.dimensions.width}
-          position={ [this.dimensions.x, 0.475, -3].join(" ") }
-        >
-        </a-box>
-
-        <a-box class="shelf-ledge" color="purple"
-          depth="0.5" height="0.05" width={this.dimensions.width}
-          position={ [this.dimensions.x, 0.95, -3].join(" ") }
-        >
-        </a-box>
-
-        <a-box class="shelf-ledge" color="purple"
-          depth="0.5" height="0.05" width={this.dimensions.width}
-          position={ [this.dimensions.x, 1.425, -3].join(" ") }
-        >
-        </a-box>
-
-        <a-box class="shelf-ledge" color="purple"
-          depth="0.5" height="0.05" width={this.dimensions.width}
-          position={ [this.dimensions.x, 1.9, -3].join(" ") }
-        >
-        </a-box>
+        {this.ledges}
 
       </a-entity>
     );
